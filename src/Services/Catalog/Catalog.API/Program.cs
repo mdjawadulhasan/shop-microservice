f74d71079c25a9c412ba05597ba0399f2a276800
mdjawadulhasan <128669123+mdjawadulhasanbs23@users.noreply.github.com>
2024-11-04 11:26:34 +0600

    Entity Created

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//Add services
-
 var app = builder.Build();
-
-//Configure HTTP Req Pipeline
-//app.MapGet("/", () => "Hello World!");
 
 app.Run();
