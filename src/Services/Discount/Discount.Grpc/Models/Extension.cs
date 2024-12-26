94f95934377050d3b8b30727c55cd96bfe3c3131
mdjawadulhasan <jawadul.hasan@brainstation-23.com>
2024-12-26 18:11:07 +0600

    Discount grpc crud service implement

---
+++ src/Services/Discount/Discount.Grpc/Models/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Models;
+
+public static class Extension
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        context.Database.Migrate();
+        return app;
+    }
+}
