6b15cfe730437b57290d3176dcc71e7cefc2ef82
mdjawadulhasan <jawadul.hasan@brainstation-23.com>
2025-01-05 18:12:26 +0600

    Migration added

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,25 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extension;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration)
+                .AddApiServices();
+
+var app = builder.Build();
+
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitDatabaseAsync();
+}
+
+app.Run();
+
+
+
---
+++ src/Services/Ordering/Ordering.Domain/Abstraction/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Ordering.Domain.Abstraction;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    public readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+
+        _domainEvents.Clear();
+
+        return dequeuedEvents;
+    }
+}
